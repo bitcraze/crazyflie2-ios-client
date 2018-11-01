df931e90733e6c117c54bf258433b90d6f7fd265
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2018-11-01 01:31:34 +0100

    Move test target to swift

---
+++ Crazyflie clientTests/Crazyflie_clientTests.swift
@@ -0,0 +1,33 @@
+//
+//  Crazyflie_clientTests.swift
+//  Crazyflie clientTests
+//
+//  Created by Andres Cecilia Luque on 01/11/2018.
+//  Copyright © 2018 Bitcraze. All rights reserved.
+//
+
+import XCTest
+
+class Crazyflie_clientTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
