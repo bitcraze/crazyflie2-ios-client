e51b08ce5dcd5b1985eb643043b9945fff16c661
Valeriy Van <github@w7software.com>
2023-01-10 15:27:23 +0200

    Fix typo in comment

--- Crazyflie client/FirmwareImage.swift
+++ Crazyflie client/FirmwareImage.swift
@@ -37,7 +37,7 @@ class FirmwareImage {
                 return
             }
             
-            // Decoding the JSON and inithializing a new FirmwareImage object
+            // Decoding the JSON and initializing a new FirmwareImage object
             let json: JSON
             do {
                 json = try JSON(data: data)
